025b817a3de8e33c6fb72c763f00bec651bc5b26
Thorlakur16 <thorlakur16@ru.is>
2018-03-20 23:25:36 +0000

    meiri lagfæringar og npm dót

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Carousel } from './Carousel';
+export { default as CartoonNetworkSpinner } from './CartoonNetworkSpinner';
+export { default as Column } from './Column';
+export { default as DatePicker } from './DatePicker';
+export { default as Modal } from './Modal';
+export { default as NameCard } from './NameCard';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Row } from './Row';
+export { default as Tab } from './Tab';
+export { default as Tabs } from './Tabs';
+export { default as TimePicker } from './TimePicker';
